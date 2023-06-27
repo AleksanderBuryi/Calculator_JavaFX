13e1464fe931bcff024bbf9b207429c95ed03d5d
Александр Бурый <a.buriy@it-minsk.by>
2023-06-27 12:37:07 +0300

    Updated

--- src/main/java/com/calculator/calculator_javafx/HelloApplication.java
+++ src/main/java/com/calculator/calculator_javafx/HelloApplication.java
@@ -3,8 +3,10 @@ package com.calculator.calculator_javafx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.*;
 import javafx.stage.Stage;
 
+import java.io.File;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
@@ -12,6 +14,8 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("Calculator.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 425);
+        Image ico = new Image(new File("src/main/resources/com/calculator/calculator_javafx/icon.png").toURI().toString());
+        stage.getIcons().add(ico);
         stage.setTitle("Calculator");
         stage.setResizable(false);
         stage.setScene(scene);
